26a01bb96304612ee165197b2f65d58a6df2a5d0
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-01 23:09:07 +0800

    Add: migration and product model

---
+++ db/migrate/20241001145414_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :quantity
+      t.float :price
+      t.boolean :available
+      t.datetime :released_at
+      t.date :expiry_date
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_01_145414) do
+  create_table "products", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "quantity"
+    t.float "price"
+    t.boolean "available"
+    t.datetime "released_at"
+    t.date "expiry_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
